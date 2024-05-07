0e71ffca4383cbd61ded0a61f2a8ce215df81c14
Oriol Almendro martí <oriolalmendromarti@Oriols-MacBook-Pro.local>
2024-05-07 02:41:24 +0200

    Include build directory in repository

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 function Account() {
     const [account, setAccount] = useState(null);
 
-    useEffect(() => {
+    useEffect(() => { // This hook will run when the component mounts
         const accountID = localStorage.getItem('accountID'); // Retrieve the account ID from the local storage
 
         fetch(`http://localhost:8080/account/${accountID}`)
